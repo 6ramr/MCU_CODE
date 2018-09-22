6dbb2039b5fd086e2e8e66c4f36a4e100f3a19a6
6ramr <kein_stress@hotmail.com>
2018-09-22 17:37:59 +0200

    CAN in closed loop established

--- Src/main.c
+++ Src/main.c
@@ -48,6 +48,8 @@
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
+CAN_HandleTypeDef hcan;
+
 SPI_HandleTypeDef hspi1;
 DMA_HandleTypeDef hdma_spi1_rx;
 
@@ -60,6 +62,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_CAN_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -90,6 +93,7 @@ int main(void)
 //	uint8_t sensorData[4]= {SENSOR_OUT_X_H, SENSOR_OUT_X_L,0,0};
 //  uint8_t sensorData[4]= {SENSOR_WHO_AM_I, SENSOR_WHO_AM_I,0,0};
 	uint8_t sensorData[6]={0};
+	uint8_t transmitData[6];
   /* USER CODE END 1 */
 
   /* MCU Configuration----------------------------------------------------------*/
@@ -105,6 +109,40 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
+//#############################
+//#  Delay necessary for clocks to spread through chip. Otherwise MX_CAN_Init() fails
+//#############################
+  HAL_Delay(1000);
+  CanTxMsgTypeDef CanTxMsg;
+  CanTxMsg.StdId = 0x00;
+  CanTxMsg.ExtId = 0x00;
+  CanTxMsg.IDE = CAN_ID_STD;
+  CanTxMsg.RTR = CAN_RTR_DATA;
+  CanTxMsg.DLC = 6;
+
+  CanRxMsgTypeDef CanRxMsg;
+  /*CanRxMsg.StdId = 0;
+  CanRxMsg.ExtId = 0;
+  CanRxMsg.IDE = CAN_ID_STD;
+  CanRxMsg.RTR = CAN_RTR_DATA;
+  CanRxMsg.DLC = 6;
+  CanRxMsg.FMI = 0;
+  CanRxMsg.FIFONumber = CAN_FIFO0;*/
+
+  CAN_FilterConfTypeDef CanFilterConf;
+  CanFilterConf.FilterIdHigh = 0x000;
+  CanFilterConf.FilterIdLow = 0x000;
+  CanFilterConf.FilterMaskIdHigh = 0x000;
+  CanFilterConf.FilterMaskIdLow = 0x000;
+  CanFilterConf.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  CanFilterConf.BankNumber = 0;
+  CanFilterConf.FilterNumber = 0;
+  CanFilterConf.FilterMode = CAN_FILTERMODE_IDLIST;
+  CanFilterConf.FilterScale = CAN_FILTERSCALE_16BIT;
+  CanFilterConf.FilterActivation = ENABLE;
+  CanFilterConf.BankNumber = 0;
+  hcan.pTxMsg = &CanTxMsg;
+  hcan.pRxMsg = &CanRxMsg;
 
   /* USER CODE END SysInit */
 
@@ -112,10 +150,16 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_SPI1_Init();
+  MX_CAN_Init();
   /* USER CODE BEGIN 2 */
   // CUBEMX-RESISTENT RECONFIG BEGIN
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-  // END
+  if(HAL_CAN_ConfigFilter(&hcan,&CanFilterConf) != HAL_OK)
+    {
+      /* Initialization Error */
+      Error_Handler();
+    }
+    // END
 
   /* Sensor Config */
 //  SPI_TxSensor(&hspi1, sensorConf, (uint16_t)LEN(sensorConf));
@@ -125,7 +169,7 @@ int main(void)
   HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4,GPIO_PIN_RESET);
   	  HAL_SPI_Transmit(&hspi1,&sensorConf[0],2,50);
   	while( hspi1.State == HAL_SPI_STATE_BUSY );
-  	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4,GPIO_PIN_SET);
+  	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4,GPIO_PIN_SET);
   	  //SPI_TxSensor(&hspi1, &sensorConf[2*i], 2);
 //  	  HAL_Delay(1000);
 
@@ -162,11 +206,40 @@ int main(void)
 	while( hspi1.State == HAL_SPI_STATE_BUSY );
 	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_4,GPIO_PIN_SET);
 
+	for (int i=0; i<6; i++) {
+		CanTxMsg.Data[i] = sensorData[i] ;
+		sensorData[i]=0;
+	}
+
+	if(HAL_CAN_Transmit(&hcan,10) != HAL_OK)
+	    {
+	      /* CAN_Transmit_Error */
+	      Error_Handler();
+	    }
+
+	if(HAL_CAN_GetState(&hcan) != HAL_CAN_STATE_READY)
+	  {
+	    return HAL_ERROR;
+	  }
+
+	if(HAL_CAN_Receive(&hcan,CAN_FIFO0,10) != HAL_OK)
+	    {
+	      /* CAN_Receive_Error */
+	      Error_Handler();
+	    }
+
+	if(HAL_CAN_GetState(&hcan) != HAL_CAN_STATE_READY)
+	  {
+	    return HAL_ERROR;
+	  }
+	for (int i=0; i<6; i++) {
+			sensorData[i] = CanRxMsg.Data[i] ;
+	}
 	sensorOutputX = ResolveAcceleration(sensorData[1],sensorData[0]);
 	sensorOutputY = ResolveAcceleration(sensorData[3],sensorData[2]);
 	sensorOutputZ = ResolveAcceleration(sensorData[5],sensorData[4]);
 
-//	HAL_Delay(1000);
+
 	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_12);
   /* USER CODE END WHILE */
 
@@ -189,13 +262,9 @@ void SystemClock_Config(void)
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL5;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -205,7 +274,7 @@ void SystemClock_Config(void)
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
@@ -226,6 +295,30 @@ void SystemClock_Config(void)
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
+/* CAN init function */
+static void MX_CAN_Init(void)
+{
+
+  hcan.Instance = CAN;
+  hcan.Init.Prescaler = 6;
+  hcan.Init.Mode = CAN_MODE_SILENT_LOOPBACK;
+  hcan.Init.SJW = CAN_SJW_2TQ;
+  hcan.Init.BS1 = CAN_BS1_4TQ;
+  hcan.Init.BS2 = CAN_BS2_2TQ;
+  hcan.Init.TTCM = DISABLE;
+  hcan.Init.ABOM = DISABLE;
+  hcan.Init.AWUM = DISABLE;
+  hcan.Init.NART = DISABLE;
+  hcan.Init.RFLM = DISABLE;
+  hcan.Init.TXFP = DISABLE;
+  HAL_Delay(100);
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
 /* SPI1 init function */
 static void MX_SPI1_Init(void)
 {
@@ -238,7 +331,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
   hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -284,13 +377,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_10, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PA4 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  /*Configure GPIO pins : PA4 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
